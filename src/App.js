d6c9a0090aab240004a6bde55140d39f15624d8b
Marlon Pena <mar1ondanie1@outlook.com>
2023-06-02 18:28:55 -0600

    Using arrays

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// A small additional note to the previous one. React allows arrays to be rendered
+// if the array contains values that are eligible for rendering (suchas as numbers and
+// strings). So the following program would work, although the result might not be what we
+// want.
+
+const App = () => {
+
+  const friends = [' Peter', ' Maya'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <p>{friends}</p>
     </div>
+
   );
-}
+
+};
 
 export default App;
